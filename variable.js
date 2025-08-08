2b190d776e6faac60d920b465f2dca7801ba0fa3
Md. Belayet Hossain <belayet.hossain6847@gmail.com>
2025-08-08 20:49:21 +0600

    explore varible and five thing need to declare a variable

---
+++ variable.js
@@ -0,0 +1,20 @@
+/*
+*variable ==> vary +able
+* 5 thins to decleare a variable 
+ */
+
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2002;
+money =500;
+
+var age;
+console.log(money);
+console.log(price);
+
+
+var age = 17;
+age = 35;
+
+console.log(age);
