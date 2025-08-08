974645a5e36a8cddf89aae232b5b2740e8e94371
Md. Belayet Hossain <belayet.hossain6847@gmail.com>
2025-08-08 21:53:05 +0600

    explore js numbers

---
+++ number.js
@@ -0,0 +1,16 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = parseInt('26');
+var applePrice2 = parseFloat('26.999');
+
+// console.log(typeof orangePrice);
+// console.log(typeof applePrice);
+// console.log(applePrice2);
+
+// console.log(orangePrice + applePrice);
+
+var first = 0.155;
+var scound = 0.1;
+var total = first + scound;
+
+console.log(total);
