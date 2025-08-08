3c12a7e9e45f615f2bc782fe8b48257689b9b382
Md. Belayet Hossain <belayet.hossain6847@gmail.com>
2025-08-08 21:34:12 +0600

    js variable data types , numaric , string , boolean

---
+++ types.js
@@ -0,0 +1,13 @@
+// string type variable
+var name = "Belayet Hossain";
+var address = "Mirpur , Dhaka";
+var county = 'bangladesh'
+
+
+// boolean type variable
+var isRich = true;
+console.log(isRich);
+
+// print the type of variable using typeof func.
+console.log(typeof isRich)
+console.log(typeof county);
